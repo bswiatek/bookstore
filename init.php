799ed88b887d23d7847fa26fa687a9b67ec6eabf
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-07-06 10:40:22 +0200

    process payment

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 
 use Bookstore\Domain\Book;
 use Bookstore\Domain\Customer;
+use Bookstore\Domain\Payer;
 use Bookstore\Domain\Customer\Basic;
 use Bookstore\Domain\Customer\Premium;
 
@@ -19,6 +20,15 @@ function checkIfValid(Customer $customer, array $books): bool {
     return $customer->getAmountToBorrow() >= count($books);
 }
 
+function processPayment(Payer $payer, float $amount) {
+    if ($payer->isExtentOfTaxes()) {
+        echo "What a lucky one...";
+    } else {
+        $amount *= 1.16;
+    }
+    $payer->pay($amount);
+}
+
 $book1 = new Book("1984", "George Orwell", 9785267006323, 12);
 $book2 = new Book("To Kill a Mockingbird", "Harper Lee", 9780061120084, 2);
 
@@ -26,5 +36,5 @@ $customer1 = new Basic(3, 'John', 'Doe', 'johndoe@mail.com');
 $customer2 = new Premium( null, 'Mary', 'Poppins', 'mp@mail.com');
 $customer3 = new Premium(7, 'James', 'Bond', '007@mail.com');
 
-var_dump(checkIfValid($customer1, [$book1]));
-var_dump(checkIfValid($customer3, [$book1]));
+processPayment($customer1, 10);
+processPayment($customer3, 10);
