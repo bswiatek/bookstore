f1899aaab432bb10dca21253c0639feda37b8d9b
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-07-19 15:13:02 +0200

    Design Pattern: Factory

--- init.php
+++ init.php
@@ -1,12 +1,6 @@
 <?php
 
-use Bookstore\Domain\Book;
-use Bookstore\Domain\Customer;
-use Bookstore\Domain\Payer;
-use Bookstore\Domain\Customer\Basic;
-use Bookstore\Domain\Customer\Premium;
-use Bookstore\Exceptions\ExceededMaxAllowedException;
-use Bookstore\Exceptions\InvalidIdException;
+use Bookstore\Domain\Customer\CustomerFactory;
 
 function autoloader($classname) {
     $lastSlash = strpos($classname, '\\') + 1;
@@ -18,38 +12,5 @@ function autoloader($classname) {
 spl_autoload_register('autoloader');
 
 
-function checkIfValid(Customer $customer, array $books): bool {
-    return $customer->getAmountToBorrow() >= count($books);
-}
-
-function processPayment(Payer $payer, float $amount) {
-    if ($payer->isExtentOfTaxes()) {
-        echo "What a lucky one...";
-    } else {
-        $amount *= 1.16;
-    }
-    $payer->pay($amount);
-}
-
-function createBasicCustomer(int $id) {
-    try {
-        return new Basic($id, "name", "surname", "email");
-    } catch (InvalidIdException $e) {
-        echo "You cannot provide a negative id\n";
-    } catch (ExceededMaxAllowedException $e) {
-        echo "No more customers are allowed\n";
-    } catch (Exception $e) {
-        echo "Unknown Exception: " . $e->getMessage();
-    }
-}
-
-$book1 = new Book("1984", "George Orwell", 9785267006323, 12);
-$book2 = new Book("To Kill a Mockingbird", "Harper Lee", 9780061120084, 2);
-
-$customer1 = new Basic(3, 'John', 'Doe', 'johndoe@mail.com');
-$customer2 = new Premium( null, 'Mary', 'Poppins', 'mp@mail.com');
-$customer3 = new Premium(7, 'James', 'Bond', '007@mail.com');
-
-createBasicCustomer(1);
-createBasicCustomer(-1);
-createBasicCustomer(55);
+CustomerFactory::factory('basic', 2, 'mary', 'poppins', 'mary@poppins.com');
+CustomerFactory::factory('premium', null, 'james', 'bond', 'james@bond.com');
