666ac06b0132192bc78e6e8a0a7b9e62ff9ed095
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-07-20 14:32:52 +0200

    Design Pattern: Anonymous function

--- init.php
+++ init.php
@@ -13,9 +13,36 @@ function autoloader($classname) {
 spl_autoload_register('autoloader');
 
 
-CustomerFactory::factory('basic', 2, 'mary', 'poppins', 'mary@poppins.com');
-CustomerFactory::factory('premium', null, 'james', 'bond', 'james@bond.com');
+function addTaxes(array &$book, $index, $percentage) {
+    $book['price'] += round($percentage * $book['price'], 2);
+};
 
-$config = Config::getInstance();
-$dbConfig = $config->get('db');
-var_dump($dbConfig);
+class Taxes {
+    public static function add(array &$book, $index, $percentage)
+    {
+        if (isset($book['price'])) {
+            $book['price'] += round($percentage * $book['price'], 2);
+        }
+    }
+    public function addTaxes(array &$book, $index, $percentage)
+    {
+        if (isset($book['price'])) {
+            $book['price'] += round($percentage * $book['price'], 2);
+        }
+    }
+}
+
+$books = [
+    ['title' => '1984', 'price' => 8.15],
+    ['title' => 'Don Quijote', 'price' => 12.00],
+    ['title' => 'Odyssey', 'price' => 3.55]
+];
+
+array_walk($books, 'addTaxes', 0.16);
+var_dump($books);
+
+array_walk($books, ['Taxes', 'add'], 0.16);
+var_dump($books);
+
+array_walk($books, [new Taxes(), 'addTaxes'], 0.16);
+var_dump($books);
