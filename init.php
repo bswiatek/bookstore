9ac7a27e6f7481caf33f5ee51588607a6d586125
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-08-22 15:16:59 +0200

    composer

--- init.php
+++ init.php
@@ -1,16 +1,7 @@
 <?php
+require ("vendor/autoload.php");
 
-use Bookstore\Domain\Customer\CustomerFactory;
 use Bookstore\Utils\Config;
-
-function autoloader($classname) {
-    $lastSlash = strpos($classname, '\\') + 1;
-    $classname = substr($classname, $lastSlash);
-    $directory = str_replace('\\', '/', $classname);
-    $filename = __DIR__ . '/src/' . $directory . '.php';
-    require_once($filename);
-}
-spl_autoload_register('autoloader');
 
 $dbConfig = Config::getInstance()->get('db');
 $db = new PDO(
