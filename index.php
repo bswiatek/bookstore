ad492b95776ec9698eb56fbcc59d0e0f452df25c
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-09-13 12:30:47 +0200

    Twig views

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Bookstore\Core\Db;
-use Bookstore\Models\BookModel;
+use Bookstore\Models\SaleModel;
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
 $twig = new Twig_Environment($loader);
 
-$bookModel = new BookModel(Db::getInstance());
-$book = $bookModel->get(1);
-$params = ['book' => $book];
-echo $twig->loadTemplate('book.twig')->render($params);
+
+$saleModel = new SaleModel(Db::getInstance());
+$sale = $saleModel->get(1);
+$params = ['sale' => $sale];
+echo $twig->loadTemplate('sale.twig')->render($params);
