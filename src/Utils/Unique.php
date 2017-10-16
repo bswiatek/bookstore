a4018d7ba14704c05e41635ac35c7d510d141dc5
Bartłomiej Świątek <bartlomiej.swiatek@nazwa.pl>
2017-10-16 14:26:03 +0200

    Tests  - basic test

--- src/Utils/Unique.php
+++ src/Utils/Unique.php
@@ -2,33 +2,11 @@
 
 namespace Bookstore\Utils;
 
-use Bookstore\Exceptions\ExceededMaxAllowedException;
-use Bookstore\Exceptions\InvalidIdException;
-
 trait Unique {
-    private static $lastId = 0;
-    private $id;
+    protected $id;
 
     public function setId($id) {
-        if ($id < 0) {
-            throw new InvalidIdException('Id cannot be negative.');
-        }
-        if (empty($id)) {
-            $this->id = ++self::$lastId;
-        } else {
-            $this->id = $id;
-            if ($id > self::$lastId) {
-                self::$lastId = $id;
-            }
-        }
-        if($this->id > 50) {
-            throw new ExceededMaxAllowedException('Max number of users is 50');
-        }
-    }
-
-    public static function getLastId(): int
-    {
-        return self::$lastId;
+        $this->id = $id;
     }
 
     public function getId(): int
