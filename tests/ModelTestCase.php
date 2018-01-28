12662a7330635f4631f185d1ce6ab00c53491d8c
Bartłomiej Świątek <emini1@swiatek.biz>
2018-01-28 21:09:20 +0100

    Model test case

---
+++ tests/ModelTestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bookstore\Tests;
+
+use Bookstore\Core\Config;
+use PDO;
+
+abstract class ModelTestCase extends AbstractTestCase {
+    protected $db;
+    protected $tables = [];
+
+    public function setUp() {
+        $config = new Config();
+
+        $dbConfig = $config->get('db');
+        $this->db = new PDO(
+            'mysql:host=sql.fcliver.nazwa.pl;dbname=fcliver_1',
+            $dbConfig['user'],
+            $dbConfig['password']
+        );
+        $this->db->beginTransaction();
+        $this->cleanAllTables();
+    }
+
+    public function tearDown() {
+        $this->db->rollBack();
+    }
+
+    protected function cleanAllTables() {
+        foreach ($this->tables as $table) {
+            $this->db->exec("delete from $table");
+        }
+    }
+}
